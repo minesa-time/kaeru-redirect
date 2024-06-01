5ce909449bc2e7681dd326f574f31a39f90d889c
İbrahim <neoibrahim.gunes@gmail.com>
2024-06-02 00:26:58 +0300

    fuck database

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
-import fs from "fs";
 import path from "path";
 
-const currentFilePath = import.meta.url;
-const currentDirPath = path.dirname(
-    currentFilePath.replace(/^file:[/][/]/, "")
-);
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 config();
 
 import * as discord from "./discord.js";
@@ -18,24 +11,16 @@ import * as storage from "./storage.js";
 const app = express();
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static("public"));
-
-const linkedRoleFilePath = path.resolve(
-    currentDirPath,
-    "./data/linked-role.json"
-);
-export function getLinkedRoleData() {
-    try {
-        const data = fs.readFileSync(linkedRoleFilePath, "utf8");
-        return JSON.parse(data);
-    } catch (err) {
-        console.error("Error reading linked-role.json:", err);
-        return {};
-    }
-}
+const linked_roles = {
+    "1246537610583609518": ["285118390031351809"],
+    "1246561256609943675": ["621158428194242560"],
+    "1246561161495973958": ["781962200582848514"],
+    "1246555145836433593": ["783219412333101086", "706049876781170709"],
+};
 
 // Function to check if the role and user ID match
 export function isRoleClaimValid(roleName, userId) {
-    const linkedRoleData = getLinkedRoleData();
+    const linkedRoleData = linked_roles;
     console.log(linkedRoleData[roleName]?.includes(userId));
     return linkedRoleData[roleName]?.includes(userId) || false;
 }
